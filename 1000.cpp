46a50909fff0efbeac86dc2df47b5c351337d445
ZhengyuWong <1004161120@cugb.edu.cn>
2021-11-13 21:39:12 +0800

    A + B = C

---
+++ 1000.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, a, b;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> a >> b;
+        cout << a + b << endl;
+        cout << "love" << endl;
+    }
+    return 0;
+}
